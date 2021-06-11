871fe0969dec5c66d511da73b7b6985f480e9a44
kimptoc <kimptoc@penguin>
2021-06-11 16:14:35 +0100

    v1

--- photo-filer.rb
+++ photo-filer.rb
@@ -1,20 +1,34 @@
 require 'find'
+require 'fileutils'
 
 class PhotoFiler
 
   def go
     puts "go go go 2b"
-    move "src", "tgt", 5
+    move "src", "tgt", 1
   end
 
   def move(src, tgt, count)
     puts "Moving #{count} files from #{src} to #{tgt}"
+    done = 0
     Find.find(src) do |path|
         # puts "#{path} - directory? #{FileTest.directory?(path)}"
         if FileTest.directory?(path)
             next
         else
-            puts "found path:#{path}, ctime:#{File.ctime(path).year}"
+            t = File.ctime(path)
+            tgt_dir_year = t.year
+            tgt_dir_month = "#{'%02d' % t.month}"
+            puts "found path:#{path}, tgt dir:#{tgt_dir_year}/#{tgt_dir_month}"
+            tgt_year_month = "#{tgt}/#{tgt_dir_year}/#{tgt_dir_month}"
+            if !FileTest.directory?(tgt_year_month)
+                FileUtils.mkdir_p tgt_year_month
+            end
+            FileUtils.move path, tgt_year_month
+            done += 1
+            if count > 0 && done >= count
+                break
+            end
         end
     end
 
